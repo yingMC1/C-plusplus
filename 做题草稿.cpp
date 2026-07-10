50e1d2d4d504eeecc5942ef7a06c8297bafe01ee
yingMC-1 <19198189385@163.com>
2026-07-10 22:56:13 +0800

    vscode

--- 做题草稿.cpp
+++ 做题草稿.cpp
@@ -3,14 +3,22 @@ using namespace std;
 const int N = 1e4 + 5;
 int n, m, k;
 int s, t;
+vector<pair<int ,int>> g[N];
+vector<int> w;
 int main()
 {
 	cin >> n >> m >> k;
 	cin >> s >> t;
-	for (int i = 1; i <= n; ++i)
+	for (int i = 1; i <= m; ++i)
 	{
 		int a, b, c;
 		cin >> a >> b >> c;
+		g[a].emplace_back(b, c);
+		g[b].emplace_back(a, c);
+		w.push_back(c);
 	}
+	w.push_back(0);
+	sort(w.begin(), w.end());
+	w.erase(unique(w.begin(), w.end()), w.end());
 	return 0;
 }	
