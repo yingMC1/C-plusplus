7f1aa1b3fa778da541b0313f87710cf07aa79736
yingMC-1 <19198189385@163.com>
2026-07-27 19:34:59 +0800

    vscode

--- 做题草稿.cpp
+++ 做题草稿.cpp
@@ -1,25 +1,39 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int i, j, k, m, n, l, o, p, x, y;
-int sum(int x, int y) {
-    int t, i, j, s1, s2, s3, s4;
-    if (x < y) {
-        t = x;
-        x = y;
-        y = t;
+using i64 = long long;
+const int N = 105;
+
+int n, m, ha, la, hb, lb, a[N][N];
+int vis[N][N];
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+bool bfs() {
+    queue<pair<int, int>> q;
+    q.push({ha, la});
+    vis[ha][la];
+    while (!q.empty()) {
+        auto [x, y] = q.front();
+        vis[x][y] = true;
+        for (int i = 1; i <= 4; i++) {
+            x += dx[i];
+            y += dy[i];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && vis[nx][ny] == 0) {
+                mp[nx][ny] = 1;
+                q.push({nx, ny});
+            }
+        }
     }
-    if (x == 2 && y == 1) return 2;
-    else if (x == 2 && y == 2) return 3;
-    s1 = x - y;
-    s2 = s1 % 4;
-    s3 = s1 / 4;
-    s4 = s3 + s2 + (x + y - s2 * 3) / 4;
-    return s4;
 }
+
 int main() {
-    int a, b, c, d;
-    cin >> a >> b;
-    cout << sum(a, b) << endl;
-    cin >> c >> d;
-    cout << sum(c, d) << endl;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++) cin >> a[i][j];
+    cin >> ha >> la >> hb >> lb;
+    bool ans = bfs();
+    if (ans) puts("YES");
+    else puts("NO");
+    return 0;
 }
