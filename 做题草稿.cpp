73b7059cb845053b540c42dfb2944e9c91383b3d
yingMC-1 <19198189385@163.com>
2026-07-31 11:29:44 +0800

    vscode

--- 做题草稿.cpp
+++ 做题草稿.cpp
@@ -13,20 +13,9 @@ template <class T> T read() {
     return f * r;
 }
 
-template <class T> void put(T x) {
-    if (x < 0) {
-        putchar('-');
-        x = -x;
-    }
-    if (x > 9) put(x / 10);
-    putchar(x % 10 + '0');
-    putchar('\n');
-}
-
 int n, g[N], s[N], b[N], g2[N], s2[N], b2[N], x1, x2, x3;
 
 int main() {
-    freopen("medal2.in", "r", stdin);
     n = read<int>();
     for (int i = 1; i <= n; i++) {
         g[i] = read<int>();
@@ -42,11 +31,11 @@ int main() {
     for (int i = 1; i <= n; i++) {
         if (g[n] == g2[i]) x1 = 1;
         else x1 = n - (upper_bound(g + 1, g + n + 1, g[i]) - g - 2);
-        if (s[n] == s2[i]) x1 = 1;
+        if (s[n] == s2[i]) x2 = 1;
         else x2 = n - (upper_bound(s + 1, s + n + 1, s[i]) - s - 2);
-        if (s[n] == s2[i]) x1 = 1;
+        if (b[n] == b2[i]) x3 = 1;
         else x3 = n - (upper_bound(b + 1, b + n + 1, b[i]) - b - 2);
-        put(min(x1, min(x2, x3)));
+        cout << min(x1, min(x2, x3)) << endl;
     }
     return 0;
 }
