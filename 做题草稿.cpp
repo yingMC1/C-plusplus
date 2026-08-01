fb7fe9172bf6acdcd3bd4bed0e98b68c00422846
yingMC-1 <19198189385@163.com>
2026-08-01 16:55:03 +0800

    重构任务配置，更新编译和重命名步骤，添加数字签名功能

--- 做题草稿.cpp
+++ 做题草稿.cpp
@@ -19,27 +19,57 @@ template <class T, class... Args> void read(T &first, Args &...rest) {
     if constexpr (sizeof...(rest) > 0) { read<T>(rest...); }
 }
 
+struct I {
+    int x, y, cnt;
+};
+
 int n, m, m1, m2, a[N][N];
+int bx, by, ex, ey;
 bool vis[N][N];
-queue<pair<int, int>> q;
+queue<I> q, ans;
+
+int bfs() {
+    int dx[8] = {m1, m1, -m1, -m1, m2, -m2, m2, -m2};
+    int dy[8] = {m2, -m2, m2, -m2, m1, m1, -m1, -m1};
+    vis[1][1] = true;
+    q.push({bx, by, 0});
+    while (!q.empty()) {
+        auto [x, y, cnt] = q.front();
+        q.pop();
+        fo(int, i, 0, <, 8, 1) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx == ex && ny == ey) return ++cnt;
+            if (nx < 0 || ny < 0 || nx > n || ny > n || vis[nx][ny] ||
+                a[nx][ny] == 0 || a[nx][ny] == 2)
+                continue;
+            q.push({ny, nx, cnt + 1});
+            vis[nx][ny] = true;
+            ans.push({ny, nx, cnt + 1});
+        }
+    }
+    return 0;
+}
 
 int main() {
     read<int, int, int, int>(n, m, m1, m2);
-    int dx[8] = {m1, m1, -m1, -m1, m2, -m2, m2, -m2};
-    int dy[8] = {m2, -m2, m2, -m2, m1, m1, -m1, -m1};
     fo(int, i, 1, <=, n, 1) {
         fo(int, j, 1, <=, m, 1) {
             read<int>(a[i][j]);
+            if (a[i][j] == 3) {
+                bx = i;
+                by = j;
+            } else if (a[i][j] == 4) {
+                ex = i;
+                ey = j;
+            }
         }
     }
-    vis[1][1] = true;
-    q.push(make_pair(1, 1));
-    while (!q.empty()) {
-        auto [x, y] = q.front();
-        q.pop();
-        fo(int, i, 0, <, 8, 1) {
-            int nx
-        }
+    fo(int, i, 1, <=, ans.size(), 1) {
+        auto [x, y, cnt] = ans.front();
+        ans.pop();
+        cout << x << ' ' << y << ' ' << cnt << endl;
     }
+    cout << bfs() << endl;
     return 0;
 }
