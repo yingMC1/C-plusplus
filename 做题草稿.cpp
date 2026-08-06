190127d609c08a4581c4b3387fbb058cb300042a
yingMC-1 <19198189385@163.com>
2026-08-06 13:44:00 +0800

    重构程序结构，更新 C++ 代码以使用标准 C++23，删除不必要的可执行文件

--- 做题草稿.cpp
+++ 做题草稿.cpp
@@ -1,33 +1,24 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using i64 = long long;
+using itn = int;
 const int N = 1e5 + 5;
 
-int n, c[N], dp[N], ans = 1;
-vector<int> e[N];
+int n, a[N];
+vector<int> g[N];
 
-void dfs(int x, int fa) {
-    dp[x] = 1;
-    int mx1 = 0, mx2 = 0;
-    for (int v : e[x]) {
-        if (v == fa) continue;
-        dfs(v, x);
-        if (c[x] == c[v]) continue;
-        dp[x] = max(dp[x], dp[v] + 1);
-        if (dp[v] + 1 > mx1) mx2 = mx1, mx1 = dp[v] + 1;
-        else if (dp[v] + 1 > mx2) mx2 = dp[v] + 1;
-    }
-    ans = max(ans, mx1 + mx2 + 1);
-}
+void dfs(int x, int fa) {}
 
 int main() {
     cin >> n;
-    for (int i = 1; i <= n; i++) cin >> c[i];
-    for (int i = 1, u, v; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
+        int u, v;
         cin >> u >> v;
-        e[u].push_back(v);
-        e[v].push_back(u);
+        g[u].push_back(v);
+        g[v].push_back(u);
     }
+    for (int i = 1; i <= n; i++) cin >> a[i];
     dfs(1, 0);
-    cout << ans;
+    return 0;
 }
