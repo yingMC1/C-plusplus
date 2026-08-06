0929b9ccd3ce29b481dea621dc9f79446ff8eaff
yingMC1 <ying110189@outlook.com>
2026-08-06 15:34:57 +0800

    vscode

--- 做题草稿.cpp
+++ 做题草稿.cpp
@@ -6,25 +6,28 @@ using itn = int;
 const int N = 200010;
 const int mod = 10007;
 
-i64 n, a[N], dp[N], ans;
-vector<int> g[N];
+i64 n, a[N], dp[N], sum;
+vector<pair<int, int>> g[N];
 
 void dfs(int x, int fa) {
-    for (int i : g[x]) {
-        if (i == fa) continue;
-        dfs(i, x);
-        }
+    dp[x] = 1;
+    for (auto [a, b] : g[x]) {
+        if (a == fa) continue;
+        dfs(a, x);
+        dp[x] += dp[a];
+        sum += b * dp[a] * (n - dp[a]);
+    }
 }
 
 int main() {
     cin >> n;
     for (int i = 1; i < n; i++) {
-        int u, v;
-        cin >> u >> v;
-        g[u].push_back(v);
-        g[v].push_back(u);
+        int u, v, w;
+        cin >> u >> v >> w;
+        g[u].push_back({v, w});
+        g[v].push_back({u, w});
     }
     dfs(1, 0);
-    cout << ans << endl;
+    cout << sum << endl;
     return 0;
 }
