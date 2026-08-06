a9216a9008e6421510dfd5d5889838286ec5f0a4
yingMC-1 <19198189385@163.com>
2026-08-06 13:24:43 +0800

    重构深度优先搜索算法，优化节点贡献计算逻辑，添加可执行文件以支持程序运行

--- 做题草稿.cpp
+++ 做题草稿.cpp
@@ -1,30 +1,33 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using i64 = long long;
-using itn = int;
-const int N = 2e5;
+const int N = 1e5 + 5;
 
-int n = 1, root, dp[N][3];
-string s;
-vector<int> g[N];
+int n, c[N], dp[N], ans = 1;
+vector<int> e[N];
 
 void dfs(int x, int fa) {
-    for (int i : g[x]) {
-        if (i == fa) continue;
-
-        dfs(i, x);
+    dp[x] = 1;
+    int mx1 = 0, mx2 = 0;
+    for (int v : e[x]) {
+        if (v == fa) continue;
+        dfs(v, x);
+        if (c[x] == c[v]) continue;
+        dp[x] = max(dp[x], dp[v] + 1);
+        if (dp[v] + 1 > mx1) mx2 = mx1, mx1 = dp[v] + 1;
+        else if (dp[v] + 1 > mx2) mx2 = dp[v] + 1;
     }
+    ans = max(ans, mx1 + mx2 + 1);
 }
 
 int main() {
-    cin >> s;
-    size_t l = s.size();
-    for (int i = 1; i <= l; i++) {
-        n += s[i];
-        root++;
-        // q[root].push_back()
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> c[i];
+    for (int i = 1, u, v; i < n; i++) {
+        cin >> u >> v;
+        e[u].push_back(v);
+        e[v].push_back(u);
     }
-    cout << 2;
-    return 0;
+    dfs(1, 0);
+    cout << ans;
 }
