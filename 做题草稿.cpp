b712f004a8210d740f453e212fe40ecd1d91eb57
yingMC1 <ying110189@outlook.com>
2026-08-06 15:13:17 +0800

    vscode

--- 做题草稿.cpp
+++ 做题草稿.cpp
@@ -3,22 +3,28 @@
 using namespace std;
 using i64 = long long;
 using itn = int;
-const int N = 1e5 + 5;
+const int N = 200010;
+const int mod = 10007;
 
-int n, a[N];
+i64 n, a[N], dp[N], ans;
 vector<int> g[N];
 
-void dfs(int x, int fa) {}
+void dfs(int x, int fa) {
+    for (int i : g[x]) {
+        if (i == fa) continue;
+        dfs(i, x);
+        }
+}
 
 int main() {
     cin >> n;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i < n; i++) {
         int u, v;
         cin >> u >> v;
         g[u].push_back(v);
         g[v].push_back(u);
     }
-    for (int i = 1; i <= n; i++) cin >> a[i];
     dfs(1, 0);
+    cout << ans << endl;
     return 0;
 }
