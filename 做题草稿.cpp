c003dd7a2ba61ecd535c49466c51d93624ce4d1f
yingMC1 <ying110189@outlook.com>
2026-08-08 23:46:49 +0800

    更新输入文件格式，重构 DFS 函数以支持多维动态规划

--- 做题草稿.cpp
+++ 做题草稿.cpp
@@ -4,17 +4,23 @@ using namespace std;
 using i64 = long long;
 const int N = 2005;
 
-int n, k, dp[N][N], sz[N];
-vector<pair<int, int>> g[N];
+// 不放、被看守、自己放
+int n, k, dp[N][N][3], f[N][N], sz[N];
+vector<int> g[N];
 
 void dfs(int x, int fa) {
     sz[x] = 1;
-    dp[x][0] = 0;
-    for (auto [i, w] : g[x]) {
+    for (int i : g[x]) {
         if (i == fa) continue;
         dfs(i, x);
+        memset(f, -0x3f, sizeof(f));
         for (int j = sz[x]; j >= 0; j--) {
-            for (int k = 0; k <= sz[i]; k++) {}
+            for (int k = 1; k <= sz[i]; k++) {
+                f[j][0] = max(f[j][0], dp[i][k][0] + dp[i][k][1]);
+                f[j][1] = max(f[j][1], dp[i][k][2]);
+                f[j][2] = max({f[j][2], dp[i][k][0], dp[i][k][1], dp[i][k][2]});
+                f[fa][j] += 1;
+            }
         }
         sz[x] += sz[i];
     }
@@ -25,12 +31,11 @@ int main() {
     cin >> n >> k;
     for (int i = 1; i <= n; i++) {
         int u, v, w;
-        cin >> u >> v >> w;
-        g[u].push_back({v, w});
-        g[v].push_back({u, w});
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
     }
     memset(dp, -0x3f, sizeof(dp));
     dfs(1, 0);
-    cout << dp[1][k] << endl;
     return 0;
 }
