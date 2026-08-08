3f8e3551abf5d3e6aadfbfbcd0f8535c4b7138ad
yingMC1 <ying110189@outlook.com>
2026-08-08 15:11:43 +0800

    重构代码，更新输入文件格式并优化 DFS 算法实现

--- 做题草稿.cpp
+++ 做题草稿.cpp
@@ -2,30 +2,37 @@
 
 using namespace std;
 using i64 = long long;
-const int mod = 1e9;
-;
+const int INF = 0x3f;
 const int N = 2005;
 
-int n, dp[N][N][3];
-vector<int> g[N];
+int n, q, dp[N][N], sz[N];
+vector<pair<int, int>> g[N];
 
 void dfs(int x, int fa) {
-
-    for (int i : g[x]) {
+    dp[x][0] = 0;
+    for (auto [i, w] : g[x]) {
         if (i == fa) continue;
         dfs(i, x);
+        for (int j = sz[x]; j >= 0; j--)
+            for (int k = 0; k <= sz[i]; k++)
+                if (dp[x][j] != -INF && dp[j][k] != -INF)
+                    dp[x][j + k + 1] =
+                        max(dp[x][j + k + 1], dp[x][j] + dp[j][k] + w);
+        sz[x] += sz[i] + 1;
     }
 }
 
 int main() {
-    freopen("input.txt", "r", stdin);
-    cin >> n;
+    freopen("input", "r", stdin);
+    cin >> n >> q;
     for (int i = 1; i < n; i++) {
-        int u, v;
-        cin >> u >> v;
-        g[u].push_back(v);
-        g[v].push_back(u);
+        int u, v, w;
+        cin >> u >> v >> w;
+        g[u].push_back({v, w});
+        g[v].push_back({u, w});
     }
+    memset(dp, -INF, sizeof(dp));
     dfs(1, 0);
+    cout << dp[1][q] << endl;
     return 0;
 }
