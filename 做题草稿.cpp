0dcc4e27d8f3957d767bbb670e1462cf6dd140db
yingMC1 <ying110189@outlook.com>
2026-08-08 23:27:02 +0800

    重构 DFS 函数，修复参数并调整常量 N 的值

--- 做题草稿.cpp
+++ 做题草稿.cpp
@@ -2,42 +2,35 @@
 
 using namespace std;
 using i64 = long long;
-const int N = 3005;
+const int N = 2005;
 
-int n, m, dp[N][N], f[N], sz[N], user[N];
+int n, k, dp[N][N], sz[N];
 vector<pair<int, int>> g[N];
 
-void dfs(int x) {
+void dfs(int x, int fa) {
+    sz[x] = 1;
     dp[x][0] = 0;
     for (auto [i, w] : g[x]) {
-        dfs(i);
+        if (i == fa) continue;
+        dfs(i, x);
         for (int j = sz[x]; j >= 0; j--) {
-            if (dp[x][j] = -0x3f) continue;
-            for (int k = 1; k <= sz[i]; k++) {
-                if (dp[i][k] == -0x3f) continue;
-                if (dp[x][j + k] < dp[x][j] + dp[i][k] - w)
-                    dp[x][j + k] < dp[x][j] + dp[i][k] - w;
-            }
+            for (int k = 0; k <= sz[i]; k++) {}
         }
-        for (int j = sz[x]; j >= 0; j--) dp[x][j] = f[j];
         sz[x] += sz[i];
     }
 }
 
 int main() {
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    cin >> n >> m;
-    for (int i = 1; i <= n - m; i++) {
-        int k;
-        cin >> k;
-        for (int j = 1; j <= k; j++) {
-            int u, w;
-            cin >> u >> w;
-            g[i].push_back({u, w});
-        }
+    freopen("input.txt", "r", stdin);
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        g[u].push_back({v, w});
+        g[v].push_back({u, w});
     }
-    for (int i = 1; i <= m; i++) cin >> user[i];
     memset(dp, -0x3f, sizeof(dp));
-    dfs(1);
+    dfs(1, 0);
+    cout << dp[1][k] << endl;
     return 0;
 }
