19427a7dd1865d6702dea87b2e210ca2d282c529
yingMC1 <ying110189@outlook.com>
2026-08-09 15:17:30 +0800

    修复代码格式和变量命名，优化 DFS 函数逻辑，调整输入输出格式

--- 做题草稿.cpp
+++ 做题草稿.cpp
@@ -1,64 +1,48 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 using i64 = long long;
-const int mod = 1000000007;
 const int N = 2005;
 
-i64 n, sz[N], dp[N][N][3], f[N][3];
-vector<int> g[N];
+i64 n, k, sz[N], dp[N][N], f[N];
+// dp[x][i]表示x这棵子树有i个黑点距离总和的最大值
+vector<pair<int, int>> g[N];
 
 void dfs(int x, int fa) {
-    memset(dp[x], 0, sizeof(dp[x]));
-    dp[x][0][0] = 1;
-    dp[x][1][2] = 1;
+    memset(dp[x], -0x3f, sizeof(dp[x]));
+    dp[x][0] = 0;
+    dp[x][1] = 0;
     sz[x] = 1;
-    for (int i : g[x]) {
+    for (auto [i, w] : g[x]) {
         if (i == fa)
             continue;
         dfs(i, x);
-        sz[x] += sz[i];
-        memset(f, 0, sizeof(f));
-        for (int a = 0; a <= sz[x]; a++) {
-            for (int j = 0; j < 3; j++) {
-                for (int b = 0; b <= sz[i]; b++) {
-                    for (int c = 0; c < 3; c++) {
-                        int cnt1 = a + b, cnt2 = j;
-                        if (j == 2) {
-                            if (c == 0)
-                                cnt1 += 1;
-                        } else if (j == 1) {
-                            if (c == 2)
-                                cnt2 += 1;
-                        } else {
-                            if (c == 2) {
-                                cnt1 += 1;
-                                cnt2 += 1;
-                            }
-                        }
-                        f[cnt1][cnt2] = (f[cnt1][cnt2] + dp[x][a][j] * dp[i][b][c]) % mod;
-                    }
-                }
+        memset(f, -0x3f, sizeof(f));
+        for (int j = 0; j <= sz[x]; j++) {
+            if (dp[x][j] < 0)
+                continue;
+            for (int t = 0; t <= sz[i]; t++) {
+                if (dp[i][t] < 0)
+                    continue;
+                f[j] = max(f[j], dp[x][j] + dp[i][t] + w * (t * (k - t) + (sz[i] - t) * ((n - sz[i]) - (k - t))));
             }
         }
-        for (int s = 0; s <= sz[x]; s++) {
-            dp[x][s][0] = f[s][0];
-            dp[x][s][1] = f[s][1];
-            dp[x][s][2] = f[s][2];
-        }
+        sz[x] += sz[i];
+        for (int j = 0; j <= sz[x]; j++)
+            dp[x][j] = f[j];
     }
 }
+
 int main() {
-    cin >> n;
-    for (int i = 1; i < n; i++) {
-        int u, v;
-        cin >> u >> v;
-        g[u].push_back(v);
-        g[v].push_back(u);
+    cin >> n >> k;
+    for (int j = 1; j < n; j++) {
+        int u, i, w;
+        cin >> u >> i >> w;
+        g[u].push_back({i, w});
+        g[i].push_back({u, w});
     }
+    memset(dp, -0x3f, sizeof(dp));
     dfs(1, 0);
-    for (int i = 0; i <= n; i++) {
-        i64 ans = (dp[1][i][0] + dp[1][i][1] + dp[1][i][2]) % mod;
-        cout << ans << '\n';
-    }
+    cout << dp[1][k] << endl;
     return 0;
 }
