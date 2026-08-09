b947701e5fdff7885b18952595e792b2cc1917e0
yingMC1 <ying110189@outlook.com>
2026-08-09 13:42:12 +0800

    vscode

--- 做题草稿.cpp
+++ 做题草稿.cpp
@@ -1,43 +1,62 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 using i64 = long long;
-const int mod = 1e9 + 7;
+const int mod = 1000000007;
 const int N = 2005;
 
-// 不放、被看守、自己放
-int n, k, dp[N][N][3], f[N][N], sz[N];
+int n, k, sz[N];
+i64 dp[N][N][3], f[N][3];
 vector<int> g[N];
 
+i64 mul(i64 a, i64 b) { return (__int128)a * b % mod; }
+
 void dfs(int x, int fa) {
+    memset(dp[x], 0, sizeof(dp[x]));
     dp[x][0][0] = 1;
     dp[x][1][2] = 1;
     sz[x] = 1;
     for (int i : g[x]) {
-        if (i == fa) continue;
+        if (i == fa)
+            continue;
         dfs(i, x);
-        for (int j = sz[x]; j >= 0; j--) {
-            for (int k = 1; k <= sz[i]; k++) {
-                f[j][0] = max(f[j][0], dp[i][k][0] + dp[i][k][1]);
-                f[j][1] = max(f[j][1], dp[i][k][2]);
-                f[j][2] = max({f[j][2], dp[i][k][0], dp[i][k][1], dp[i][k][2]});
-                dp[fa][j][0] += dp[fa][j][1];
-                dp[i][k][0] = dp[i][k][1];
+        memset(f, 0, sizeof(f));
+        for (int a = 0; a <= sz[x]; a++) {
+            for (int j = 0; j < 3; j++) {
+                if (!dp[x][a][j])
+                    continue;
+                for (int b = 0; b <= sz[i]; b++) {
+                    int sum = a + b;
+                    for (int c = 0; c < 3; c++) {
+                        if (!dp[i][b][c])
+                            continue;
+                        i64 val = mul(dp[x][a][j], dp[i][b][c]);
+                        if (j == 0) {
+                            if (c == 0)
+                                f[sum][1] = (f[sum][1] + val) % mod;
+                            else
+                                f[sum][0] = (f[sum][0] + val) % mod;
+                        } else if (j == 1)
+                            f[sum][2] = (f[sum][2] + val) % mod;
+                        else if (j == 2)
+                            f[sum][0] = (f[sum][0] + val) % mod;
+                    }
+                }
             }
         }
-        for (int j = sz[x]; j >= 0; j--) {
-            dp[x][j][0] = f[j][0];
-            dp[x][j][1] = f[j][1];
-            dp[x][j][2] = f[j][2];
-        }
         sz[x] += sz[i];
+        for (int s = 0; s <= sz[x]; s++) {
+            dp[x][s][0] = f[s][0];
+            dp[x][s][1] = f[s][1];
+            dp[x][s][2] = f[s][2];
+        }
     }
 }
-
 int main() {
-    freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     cin >> n >> k;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n - 1; i++) {
         int u, v;
         cin >> u >> v;
         g[u].push_back(v);
@@ -45,7 +64,7 @@ int main() {
     }
     dfs(1, 0);
     for (int i = 0; i <= n; i++) {
-        i64 ans = (dp[1][k][0] + dp[1][k][1] + dp[1][k][2]) % mod;
+        i64 ans = (dp[1][i][0] + dp[1][i][1] + dp[1][i][2]) % mod;
         cout << ans << endl;
     }
     return 0;
