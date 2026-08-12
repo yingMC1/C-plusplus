c625b4b796ab5b77cf8453ed7702ff7176021c08
yingMC1 <ying110189@outlook.com>
2026-08-12 15:28:42 +0800

    更新输入文件格式，重构主函数逻辑，优化树的更新与查询功能

--- 做题草稿.cpp
+++ 做题草稿.cpp
@@ -1,69 +1,44 @@
 #include <bits/stdc++.h>
 
+#define lowbit(x) ((x) & (-(x)))
+#define endl '\n'
 using namespace std;
 using i64 = long long;
-const int N = 100005;
+const int N = 5e5 + 5;
 
-vector<int> g[N];
-int dep[N];
-int f[N][20];
-int x[N], y[N];
+int n, m, tree[N];
 
-void dfs(int x, int fa) {
-    dep[x] = dep[fa] + 1;
-    f[x][0] = fa;
-    for (int i = 1; i <= 19; i++)
-        f[x][i] = f[f[x][i - 1]][i - 1];
-    for (auto i : g[x])
-        if (i != fa)
-            dfs(i, x);
+void update(int x, int w) {
+    while (x <= n) {
+        tree[x] += w;
+        x += lowbit(x);
+    }
 }
 
-int lca(int x, int y) {
-    if (dep[x] < dep[y])
-        swap(x, y);
-    for (int i = 19; i >= 0; i--)
-        if (dep[f[x][i]] >= dep[y])
-            x = f[x][i];
-    if (x == y)
-        return x;
-    for (int i = 19; i >= 0; i--)
-        if (f[x][i] != f[y][i])
-            x = f[x][i], y = f[y][i];
-    return f[x][0];
+i64 query(int x) {
+    i64 sum = 0;
+    while (x) {
+        sum += tree[x];
+        x -= lowbit(x);
+    }
+    return sum;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int n, q;
-    cin >> n >> q;
-    vector<pair<int, int>> tree;
-    int tot = 0;
-    for (int i = 1; i <= n - 1; i++) {
-        int u, v;
-        cin >> u >> v;
-        tree.push_back({u, v});
-        y[++tot] = u;
-        y[++tot] = v;
+    freopen("input.txt", "r", stdin);
+    cin >> n >> m;
+    while (m--) {
+        int op;
+        cin >> op;
+        if (op == 1) {
+            int x;
+            cin >> x;
+            update(x, 1);
+        } else {
+            int l, r;
+            cin >> l >> r;
+            cout << query(r) - query(l - 1) << endl;
+        }
     }
-    sort(y + 1, y + tot + 1);
-    int cnt = unique(y + 1, y + tot + 1) - (y + 1);
-    for (auto e : tree) {
-        int u = lower_bound(y + 1, y + cnt + 1, e.first) - y;
-        int v = lower_bound(y + 1, y + cnt + 1, e.second) - y;
-        g[u].push_back(v);
-        g[v].push_back(u);
-    }
-    dfs(1, 1);
-    while (q--) {
-        int a, b;
-        cin >> a >> b;
-        int u = lower_bound(y + 1, y + cnt + 1, a) - y;
-        int v = lower_bound(y + 1, y + cnt + 1, b) - y;
-        int L = lca(u, v);
-        cout << dep[u] + dep[v] - 2 * dep[L] << endl;
-    }
-
     return 0;
 }
