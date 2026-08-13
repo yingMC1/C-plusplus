de2a5cd1b8546c26e5b0e45a4507cbd1126b9047
yingMC1 <ying110189@outlook.com>
2026-08-13 14:43:27 +0800

    重构代码，增加树状数据结构，优化更新与查询功能

--- 做题草稿.cpp
+++ 做题草稿.cpp
@@ -4,13 +4,47 @@
 #define endl '\n'
 using namespace std;
 using i64 = long long;
-const int N = 1e5 + 5;
+const int N = 300005;
 
-int n, l, r, a[N];
+int n, m, a[N];
+
+struct bit {
+    int tree[N];
+    void update(int x, int w) {
+        while (x <= n) {
+            tree[x] ^= w;
+            x += lowbit(x);
+        }
+    }
+    int query(int x) {
+        int sum = 0;
+        while (x) {
+            sum ^= tree[x];
+            x -= lowbit(x);
+        }
+        return sum;
+    }
+} t;
 
 int main() {
-    cin >> n >> l >> r;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
     for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        t.update(i, a[i] ^ a[i - 1]);
+    }
+
+    while (m--) {
+        int op, x, y;
+        cin >> op >> x >> y;
+        if (op == 1) {
+            t.update(x, y);
+            a[x] ^= y;
+        } else {
+            cout << (t.query(y) ^ t.query(x - 1)) << endl;
+        }
     }
     return 0;
 }
